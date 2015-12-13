797cc1d17223fe7db0a56de2fb0d96129bff3db8
Christian Lehner <lehner.chri@gmail.com>
2015-12-13 16:15:34 +0100

    changed mongodb filter script

--- mongoDB filter/filter.js
+++ mongoDB filter/filter.js
@@ -43,23 +43,29 @@ function map() {
 				tweetHashtags.push(this.entities.hashtags[objectIndex].text)
 			}
 
-			var place = null
-			if(this.place) {
-				place = {
-					country_code: this.place.country_code,
-					country: this.place.country,
-					name: this.place.full_name
-				}
+			var lng = 0;
+			var lat = 0;
+
+			// this.coordinates is longitude first, then latitude
+			// this.geo (deprecated) seems to be vice versa
+			if(this.coordinates) {
+				lng = this.coordinates.coordinates[0];
+				lat = this.coordinates.coordinates[1];
 			}
+			else if(this.geo) {
+				lng = this.geo.coordinates[1];
+				lat = this.geo.coordinates[0];	
+			}
+
 			emit(
 				this._id,
 				{
 					_id: this._id,
 					text: this.text,
-					created_at: this.created_at,
-					place: place,
+					created_at: new Date(this.created_at),
 					hashtags: tweetHashtags,
-					coordinates: this.coordinates,
+					longitude: lng,
+					latitude: lat,
 					lang: this.lang
 				}
 			)
@@ -77,9 +83,19 @@ db.tweets.mapReduce(map, reduce, {
 			{ lang: "en" },
 			{ lang: "de"}
 		],
-		geo: {
-			$ne: null
-		}
+		$or: [
+			{ 
+				geo: {
+					$ne: null
+				}
+			},
+			{
+				coordinates: {
+					$ne: null
+				}
+			}
+		]
 	},
-	out: "filteredTweets"
+	out: "filteredTweets",
+	verbose: true
 });
