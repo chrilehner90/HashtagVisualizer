5591e525f6c83350e03556b6d733da4c3be202f2
Fabio Batz <fbatz.mmt-m2014@fh-salzburg.ac.at>
2016-01-13 18:23:02 +0100

    changed index.js to app.js

---
+++ WebDashboard/app/app.js
@@ -0,0 +1,29 @@
+let express = require("express"),
+    mongodb = require("mongodb").MongoClient,
+    bodyParser = require("body-parser");
+
+var app = express();
+
+app.set("view engine", "jade");
+app.set("views", __dirname + "/views");
+//app.use(bodyParser)
+
+mongodb.connect("mongodb://localhost:27017/twitter-database", function(err, db) {
+	if(err) throw err;
+
+	console.log("Server listening to localhost:3000");
+
+	app.get("/", function(req, res) {
+
+		// var tweets = db.collection("tweets").find().toArray();
+
+		res.render("../views/index");
+
+		// tweets.then(function(tweets) {
+		// 	res.render("views/index", { tweets: tweets });
+		// });
+	})
+
+
+	app.listen(3000);
+});
