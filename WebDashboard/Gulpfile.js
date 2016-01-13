5b8ef8704ba7163fd90f8a694c78e950317497f1
Christian Lehner <lehner.chri@gmail.com>
2016-01-13 17:15:02 +0100

    added livereload

--- WebDashboard/Gulpfile.js
+++ WebDashboard/Gulpfile.js
@@ -1,13 +1,24 @@
-var gulp = require("gulp")
-var babel = require("gulp-babel")
-var concat = require("gulp-concat")
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var livereload = require("gulp-livereload");
 
 
-gulp.task("default", function() {
+gulp.task("js", function() {
   gulp.src("app/**/*.js")
     .pipe(babel({
       presets: ["es2015"]
     }))
     .pipe(concat("app.js"))
     .pipe(gulp.dest("build/js"))
+    .pipe(livereload());
+
 });
+
+gulp.task("watch", function() {
+  livereload.listen();
+  gulp.watch("app/**/*.js", ["js"]);
+});
+
+
+gulp.task("default", ["js", "watch"]);
