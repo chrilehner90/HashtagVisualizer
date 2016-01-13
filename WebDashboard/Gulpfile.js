49df658a3707dacedd2fc8cf27e461c53f5ade0d
Christian Lehner <lehner.chri@gmail.com>
2016-01-13 21:10:33 +0100

    changed paths in gulpfile

--- WebDashboard/Gulpfile.js
+++ WebDashboard/Gulpfile.js
@@ -5,6 +5,18 @@ var livereload = require("gulp-livereload");
 var del = require("del");
 var nodemon = require("gulp-nodemon");
 
+var jsSourceFiles = {
+	client: {
+		app: "app/client/**/*.js",
+		plugins: [
+			"node_modules/angular/angular.min.js"
+		]
+	},
+	server: [
+		"app/server.js"
+	]
+}
+
 gulp.task("clean", function(){
 	del(["build"]);
 })
@@ -15,15 +27,29 @@ gulp.task("jade", function(){
 		.pipe(livereload());
 })
 
-gulp.task("js", function() {
-  gulp.src("app/**/*.js")
+gulp.task("jsClient", function() {
+  gulp.src(jsSourceFiles.client.app)
     .pipe(babel({
       presets: ["es2015"]
     }))
-    .pipe(concat("server.js"))
-    .pipe(gulp.dest("build"))
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("build/client"))
     .pipe(livereload());
+});
 
+gulp.task("jsPlugins", function() {
+	gulp.src(jsSourceFiles.client.plugins)
+		.pipe(concat("plugins.js"))
+		.pipe(gulp.dest("build/client"))
+})
+
+gulp.task("jsServer", function() {
+	gulp.src(jsSourceFiles.server)
+		.pipe(babel({
+			presets: ["es2015"]
+		}))
+		.pipe(gulp.dest("build"))
+		.pipe(livereload());
 });
 
 gulp.task("watch", function() {
@@ -41,4 +67,4 @@ gulp.task("nodemon", function(){
 })
 
 
-gulp.task("default", ["clean", "js", "jade", "nodemon", "watch"]);
+gulp.task("default", ["clean", "jsClient", "jsPlugins", "jsServer", "jade", "nodemon", "watch"]);
