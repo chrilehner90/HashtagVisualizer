6bbfef217c1f626d3f1f0bfe096d5c154dc4a6a0
Christian Lehner <lehner.chri@gmail.com>
2016-01-13 17:21:44 +0100

    added jade + bodyparser

--- WebDashboard/app/index.js
+++ WebDashboard/app/index.js
@@ -1,3 +1,13 @@
-let express = require("express")
-let mongodb = require("mongodb")
+let express = require("express"),
+    mongodb = require("mongodb").MongoClient,
+    bodyParser = require("body-parser")
 
+var app = express()
+
+app.set("view-engine", "jade");
+app.set("views", __dirname + "/views");
+//app.use(bodyParser)
+
+MongoClient.connect("mongodb://localhost:27017/twitter-database", function(err, db) {
+
+})
