e8eecf0622b0f1caf9a0befeb62073b34c83387f
Christian Lehner <lehner.chri@gmail.com>
2016-01-15 17:00:48 +0100

    added stylesheet + angular MapController + APIFactory

--- WebDashboard/app/client/app.js
+++ WebDashboard/app/client/app.js
@@ -1,1 +1,61 @@
-let app = angular.module("HashtagVisualizer", [])
+let app = angular.module("HashtagVisualizer", ["ngResource"]);
+
+class APIFactory {
+  constructor($resource, $q) {
+    this.$resource = $resource;
+    this.$q = $q;
+  }
+
+  getTweets() {
+    console.log("resource", this.$resource)
+    console.log("resource", this.$q)
+
+    let deferred = this.$q.defer();
+
+    let TweetResource = this.$resource("/tweets");
+    TweetResource.query(function(tweets) {
+      deferred.resolve(tweets);
+    });
+
+    return deferred.promise;
+  }
+
+  static createClass($resource, $q) {
+    return new APIFactory($resource, $q)
+  }
+}
+
+class MapController {
+  constructor(APIFactory) {
+    this.APIFactory = APIFactory;
+    this.map = L.map('map').fitWorld();
+
+    L.Icon.Default.imagePath = "./";
+
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+
+    this.getTweets();
+
+  }
+
+  getTweets() {
+    let promise = this.APIFactory.getTweets();
+
+    let map = this.map;
+    promise.then(function(tweets) {
+      for(let tweet of tweets) {
+        L.marker([tweet.value.latitude, tweet.value.longitude]).addTo(map);
+      }
+    });
+
+
+  }
+}
+
+APIFactory.createClass.$inject = ["$resource", "$q"];
+
+app.factory("APIFactory", APIFactory.createClass);
+app.controller("MapController", ["APIFactory", MapController]);
